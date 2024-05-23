44d3cc8848a8796c548fe803a699aa3dcbd78f63
Akiva <bloch.akiva@gmail.com>
2024-05-23 06:36:40 +0300

    fixes

--- home.nix
+++ home.nix
@@ -12,12 +12,7 @@ in
   home.stateVersion = "23.11";
 
   programs.home-manager.enable = true;
-  home.packages = with pkgs; [
-    bash git kitty tmux tmate pet gum fzf vim silver-searcher bat less curl wget axel dua liquidprompt obsidian dropbox rambox github-cli stow
-  ] ++ lib.optionals isLinux [ 
-    google-chrome waybar mako libnotify rofi-wayland tofi
-  ] ++ lib.optionals isDarwin [
-  ];
+  home.packages =  with pkgs; [bash git kitty tmux tmate pet gum fzf vim silver-searcher bat less curl wget axel dua liquidprompt obsidian dropbox rambox github-cli chezmoi kitty pinentry bitwarden-cli neovim fx ] ++ lib.optionals isLinux [ google-chrome libnotify rofi-wayland tofi pinentry ] ++ lib.optionals isDarwin [ ];
   programs.bash.enable = if isLinux then true else false;
   programs.wofi.enable = if isLinux then true else false;
   programs.git = {
@@ -27,7 +22,8 @@ in
   };
   home.shellAliases = {
     l = "ls -alh";
-    nrb = if isLinux then "sudo nixos-rebuild switch --flake ~/personal-projects#default --impure" else "sudo darwin-restart switch --flake ~/personal-projects#default --impure";
+    nixswitch = if isLinux then "sudo nixos-rebuild switch --flake ~/personal-projects/nix-config#default --impure" else "sudo darwin-restart switch --flake ~/personal-projects/nix-config#default --impure";
+    nixcheck = if isLinux then "sudo nixos-rebuild test --flake ~/personal-projects/nix-config#default --impure" else "sudo darwin-restart test --flake ~/personal-projects/nix-config#default --impure";
   };
   wayland.windowManager.hyprland.enable = if isLinux then true else false;
 }
--- system.nix
+++ system.nix
@@ -12,7 +12,11 @@ in
     listenAddresses = [ {addr = "0.0.0.0"; port = 22;}];
     startWhenNeeded = true;
   };
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/etc/tailscaleAuth";
+  };
+  
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
@@ -22,9 +26,9 @@ in
   ];
   nix.extraOptions = '' experimental-features = nix-command flakes '';
   hardware.opengl.enable = true;
+  services.displayManager.sddm.enable = if isLinux then true else null;
   services.xserver = if isLinux then {
     enable = true;
-    displayManager.sddm.enable = true ;
     windowManager.hypr.enable = true;
   } else {} ;
 }
